bc2db6d6dd6048d94796915dfb427d8eb1bacf2a
arthurjeh <90380288+arthurjeh@users.noreply.github.com>
2021-10-22 14:40:31 +0200

    Add files via upload

--- Uplomulter.js
+++ Uplomulter.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const multer = require('multer');
+const fs=require('fs');
+const formidable = require('formidable')
+
+
+
 
 
 const app = express();
 
 app.use(express.json())
 
+
 //disk storage
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads')
     },
-   
     filename: function (req, file, cb) {
       cb(null, ""+Date.now())
     }
@@ -25,11 +30,16 @@ app.get('/',async(req,res)=>{
     res.sendFile(__dirname + '/index.html');
   });
 
- 
 
+
+  
   app.post('/uploadfile',upload.single('myFile'), async (req, res, next) => {       //upload to upload in particular directory
     const file = req.file           //recup the data
     console.log(file)
+    console.log(file.originalname)
+  
+
+
     if (!file) {
       const error = new Error('upload a file')
       return next(error)    //middleware
@@ -39,4 +49,27 @@ app.get('/',async(req,res)=>{
   })
 
 
-app.listen(3000, () => console.log('Server started on port 3000'))
+
+  app.post('/delete', (req, res, next) => {
+    const form = formidable({ multiples: true });
+   
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      else{
+      res.json({ fields, files });
+    }
+     const toDelete="./pictures/"+files.delete.name;
+      console.log(toDelete)
+      next()
+      
+      fs.unlink(toDelete,function(){
+        console.log("file deleted")
+      })
+    }); 
+  });
+
+  
+app.listen(8001, () => console.log('Server started on port 3000'))
