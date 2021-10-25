4f683c5ee999fcc954e8894a37e2bd2ad0da9545
arthurjeh <90380288+arthurjeh@users.noreply.github.com>
2021-10-25 09:09:37 +0200

    Add files via upload

--- Uplomulter.js
+++ Uplomulter.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const multer = require('multer');
 const fs=require('fs');
-const formidable = require('formidable')
-
-
-
-
-
+const formidable = require('formidable');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+
 
 app.use(express.json())
 
@@ -29,9 +30,6 @@ app.use(express.json())
 app.get('/',async(req,res)=>{
     res.sendFile(__dirname + '/index.html');
   });
-
-
-
   
   app.post('/uploadfile',upload.single('myFile'), async (req, res, next) => {       //upload to upload in particular directory
     const file = req.file           //recup the data
@@ -70,6 +68,33 @@ app.get('/',async(req,res)=>{
       })
     }); 
   });
+
+
+
+app.post("/rename",(req,res)=>{
+  var rename=req.body.rename
+  console.log(rename)
+
+  var toRename=req.body.toRename
+  console.log(toRename)
+  
+  fs.rename(toRename, rename, () => {
+  console.log("\nFile Renamed!\n");
+
+  });
+  
+  function getCurrentFilenames() {
+  console.log("Current filenames:");
+  fs.readdirSync(__dirname).forEach(file => {
+    console.log(file);
+  });
+  }
+  res.send("file rename")
+  
+})
+
+
+
 
   
 app.listen(8001, () => console.log('Server started on port 3000'))
