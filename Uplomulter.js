5874fc163dfbc2aeab3d38d1b33f1e17c90b3366
arthurjeh <90380288+arthurjeh@users.noreply.github.com>
2021-10-27 11:30:44 +0200

    Add files via upload

--- Uplomulter.js
+++ Uplomulter.js
@@ -13,12 +13,12 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 
 
-//disk storage
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+//disk storage ( constructor of multer)
+  const storage = multer.diskStorage({      //multer is use for upload the file
+    destination: function (req, file, cb) {//destination to the file
       cb(null, 'uploads')
     },
-    filename: function (req, file, cb) {
+    filename: function (req, file, cb) {        //filename of the new file
       cb(null, ""+Date.now())
     }
   })
@@ -36,36 +36,41 @@ app.get('/',async(req,res)=>{
     console.log(file)
     console.log(file.originalname)
   
-
-
     if (!file) {
       const error = new Error('upload a file')
       return next(error)    //middleware
     }
-      res.send("The images is upload")
+    const seeFile=JSON.parse(fs.readFileSync(file.originalname))     //parse the json file to see on the page
+    res.send(seeFile)
     
   })
 
 
 
   app.post('/delete', (req, res, next) => {
-    const form = formidable({ multiples: true });
+    const form = formidable({ multiples: true });   //use formidable to have the name and other data of the file 
    
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, (err, fields, files) => {   //parse the json file
       if (err) {
         next(err);
         return;
       }
       else{
-      res.json({ fields, files });
+      let fi=JSON.parse(fs.readFileSync(files.delete.name))     //parse the json file to see on the page
+      res.send(fi)
+      next()          //middleware
     }
-     const toDelete="./pictures/"+files.delete.name;
-      console.log(toDelete)
-      next()
+
+    const path=fs.realpathSync(files.delete.name)    //find the directory
+      console.log(path)
       
-      fs.unlink(toDelete,function(){
-        console.log("file deleted")
-      })
+     const toDelete=path
+     console.log("var to delete :"+toDelete)
+     next()
+      
+     fs.unlink(toDelete,function(){      //unlink can delete the file
+     console.log("file deleted")
+     })
     }); 
   });
 
@@ -74,27 +79,14 @@ app.get('/',async(req,res)=>{
 app.post("/rename",(req,res)=>{
   var rename=req.body.rename
   console.log(rename)
-
   var toRename=req.body.toRename
   console.log(toRename)
-  
   fs.rename(toRename, rename, () => {
   console.log("\nFile Renamed!\n");
-
   });
-  
-  function getCurrentFilenames() {
-  console.log("Current filenames:");
-  fs.readdirSync(__dirname).forEach(file => {
-    console.log(file);
-  });
-  }
-  res.send("file rename")
-  
+   res.send("file rename")
 })
 
 
 
-
-  
-app.listen(8001, () => console.log('Server started on port 3000'))
+app.listen(8001, () => console.log('Server started on port 8001'))
